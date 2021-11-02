e23eeac763121f7f07d53b25a9572f227e674110
김준기 <gugi329@daum.net>
2021-11-02 23:55:55 +0900

    Intent 연습  - 복귀를 Intent로 만들면 -> 화면이 누적으로 쌓인다 (문제 발생)

--- app/src/main/java/com/neppplus/intent_20211102/OtherActivity.kt
+++ app/src/main/java/com/neppplus/intent_20211102/OtherActivity.kt
@@ -1,11 +1,23 @@
 package com.neppplus.intent_20211102
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+//        복귀버튼 눌리면 -> 메인화면으로 이동
+
+        btnReturnToMain.setOnClickListener {
+
+            val myIntent = Intent( this, MainActivity::class.java )
+            startActivity(myIntent)
+
+        }
+
     }
 }
