3c2f2c9da3a528d38b0e07929f402f82379c6ab6
김준기 <gugi329@daum.net>
2021-11-03 22:01:08 +0900

    닉네임 변경화면 - 확인버튼  - 입력된 값 저장  - Intent에 값 첨부  - 결과 설정 (확인/ Intent)  - finish()

--- app/src/main/java/com/neppplus/intent_20211102/EditNicknameActivity.kt
+++ app/src/main/java/com/neppplus/intent_20211102/EditNicknameActivity.kt
@@ -1,11 +1,33 @@
 package com.neppplus.intent_20211102
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+
+        btnOk.setOnClickListener {
+
+//            1. 입력된 닉네임? 변수에 저장
+            val inputNickname = edtNickname.text.toString()
+
+//            2. 닉네임을 들고 가야함. => (결과를) 담아줘야함. => Intent 추가 활용
+            val resultIntent = Intent() // 왕복이고, 데이터를 담기 위한 목적만 있음. 출발/도착지 명시 X
+
+//            실제 데이터 첨부. putExtra
+            resultIntent.putExtra("nick", inputNickname)
+
+//            최종 결과 ( 확인누름, 들고 돌아갈 데이터) 지정
+            setResult(RESULT_OK, resultIntent)
+
+//            실제 이전화면 복귀
+            finish()
+
+        }
+
     }
 }
