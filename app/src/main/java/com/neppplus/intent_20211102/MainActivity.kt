2f3f735ad947d0aae191b388c778ea4dd650261c
김준기 <gugi329@daum.net>
2021-11-03 22:27:39 +0900

    카카오톡 플레이스토어 다운로드 링크

--- app/src/main/java/com/neppplus/intent_20211102/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20211102/MainActivity.kt
@@ -93,6 +93,12 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
         }
 
+        btnKakaoStore.setOnClickListener {
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+            val myIntent = Intent( Intent.ACTION_VIEW, myUri )
+            startActivity(myIntent)
+        }
+
     }
 
 
