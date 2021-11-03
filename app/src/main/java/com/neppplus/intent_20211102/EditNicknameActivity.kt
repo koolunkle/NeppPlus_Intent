6ce47ae10e7d42afea57da937e1678ae2377d532
김준기 <gugi329@daum.net>
2021-11-03 21:49:24 +0900

    닉네임 입력화면 추가

---
+++ app/src/main/java/com/neppplus/intent_20211102/EditNicknameActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.intent_20211102
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname)
+    }
+}
